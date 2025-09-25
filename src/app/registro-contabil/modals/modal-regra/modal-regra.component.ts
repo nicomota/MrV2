c41d9c1dca0213edbc0ffbcb53437a0963d56b26
Nicolas Mota <nicolas.mota2000@gmail.com>
2025-09-25 11:44:49 -0300

    ajustes açõe e conta

--- src/app/registro-contabil/modals/modal-regra/modal-regra.component.ts
+++ src/app/registro-contabil/modals/modal-regra/modal-regra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal-regra',
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-regra.component.scss']
 })
 export class ModalRegraComponent implements OnInit {
+  @Input() modalAberto: boolean = false;
+  @Input() modoAssociacaoConta: boolean = false;
+  @Input() lancamentoSelecionadoIndex: number | null = null;
+  @Input() dadosLancamentos: any[] = [];
+  @Input() contaContabilInput: string = '';
+  @Input() historicoInput: string = '';
+
+  @Output() fecharModal = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  fecharModalRegra(): void {
+    this.fecharModal.emit();
+  }
+
+  getDataLancamentoSelecionado(): string {
+    if (this.lancamentoSelecionadoIndex !== null) {
+      return this.dadosLancamentos[this.lancamentoSelecionadoIndex]?.data || '';
+    }
+    return '';
+  }
+
+  getValorLancamentoSelecionado(): string {
+    if (this.lancamentoSelecionadoIndex !== null) {
+      return this.dadosLancamentos[this.lancamentoSelecionadoIndex]?.valor || '';
+    }
+    return '';
+  }
 }
