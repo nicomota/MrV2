52030bb466bfba9342950ec393d4dad44951e98b
Nicolas Mota <nicolas.mota2000@gmail.com>
2025-09-25 12:03:06 -0300

    ajuste associar conta

--- src/app/registro-contabil/modals/modal-associacao-conta/modal-associacao-conta.component.ts
+++ src/app/registro-contabil/modals/modal-associacao-conta/modal-associacao-conta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal-associacao-conta',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-associacao-conta.component.scss']
 })
 export class ModalAssociacaoContaComponent implements OnInit {
+  @Input() modalAberto: boolean = false;
+  @Input() lancamentoSelecionadoIndex: number | null = null;
+  @Input() dadosLancamentos: any[] = [];
+  @Input() contaContabilInput: string = '';
+  @Input() historicoInput: string = '';
+
+  @Output() fecharModalEvento = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  fecharModal(): void {
+    this.fecharModalEvento.emit();
+  }
+
+  getDataLancamentoSelecionado(): string {
+    if (this.lancamentoSelecionadoIndex !== null) {
+      return this.dadosLancamentos[this.lancamentoSelecionadoIndex]?.data || '';
+    }
+    return '';
+  }
+
+  getValorLancamentoSelecionado(): string {
+    if (this.lancamentoSelecionadoIndex !== null) {
+      return this.dadosLancamentos[this.lancamentoSelecionadoIndex]?.valor || '';
+    }
+    return '';
+  }
 }
